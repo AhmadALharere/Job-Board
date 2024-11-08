ea174020a014b08e60eae44f167178b419837cdb
AhmadAL <ite.ahmadalharere@gmail.com>
2024-11-08 11:19:49 +0300

    fixed pagination

--- Jobs/views.py
+++ Jobs/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Jobs
+from django.core.paginator import Paginator
 # Create your views here.
 
 def Show_job(request):
     list_of_jobs = Jobs.objects.all()
-    context = {'Job':list_of_jobs}
-    return render(request,'Jobs/show Jobs.html',context)
+    paginator = Paginator(list_of_jobs,1)
+    pageNumber = request.GET.get('page')
+    pag_obj = paginator.get_page(pageNumber)
+    return render(request,'Jobs/show Jobs.html',{'Job':pag_obj})
 
 def show_job_details(request, id):
     jobDetail = Jobs.objects.get(id=id)
